83d9927a52e9a282562ba511f35ff2fe68a0edff
Benjamin Zagorsky <iamzags@gmail.com>
2020-04-23 16:54:27 -0400

    adds requirements to setup

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name = "integratedblog",
     version = "0.1",
-    packages = find_packages()
+    packages = find_packages(),
+    install_requires = [
+        "Flask>=0.11.1",
+        "oauth2client>=4.0.0",
+        "BeautifulSoup4>=4.5.0",
+        "markdown2>=2.3.8",
+        "peewee>=3.13.2"
+    ]
 )
